cba96d777b91da458c5640c679e26e58580a935a
Claye_L <claye@ixcys.fr>
2020-01-24 01:56:21 +0100

    added coloring characters

--- demo.py
+++ demo.py
@@ -4,15 +4,17 @@ from draw import draw_string,draw_country_ball
 country_size =(18,18)
 character_size = (13,16)
 
-img = Image.new('RGBA',(110,140),ImageColor.getcolor('white','RGBA'))
+img = Image.new('RGBA',(140,220),ImageColor.getcolor('black','RGBA'))
 
-country_codes = ['ee','fr','nl','se','se']
-names = ['vinss','claye','thevug','eljest','hannah']
+country_codes = ['ee','fr','nl','se','se','ar','ru','se','se','us','us']
+names = ['vinss','claye','thevug','eljest','hannah','metalgio','raintnt','smolbean','emma','kevin','staar']
+white = (255,255,255)
 
-row_offset = 28
+row_spacing = 20
 for i in range(len(names)):
     country = country_codes[i]
     name = names[i]
-    draw_country_ball(img,(4,i * row_offset),country)
-    draw_string(img,(26,i*row_offset),name)
+    draw_country_ball(img,(4,i * row_spacing),country)
+    draw_string(img,(26,i*row_spacing),name,white)
 img.save('demo.png')
+img.show()
--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageColor
 import os 
+import numpy as np
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\genImages"
 transparent = (0,0,0,0)
 
@@ -11,8 +12,20 @@ def get_character_path(char):
 def get_country_path(country):
     return dir_path + "\\countries\\" + country +'.png'
 
-def draw_character(image, xy, char):
+def replace_black(image, color):
+    #code copied from https://stackoverflow.com/questions/3752476/python-pil-replace-a-single-rgba-color
+    data = np.array(image)   # "data" is a height x width x 4 numpy array
+    red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
+    # Replace white with red... (leaves alpha values alone...)
+    black_areas = (red == 0) & (blue == 0) & (green == 0)
+    data[..., :-1][black_areas.T] = color[0],color[1],color[2] # Transpose back needed
+    im2 = Image.fromarray(data)
+    return im2
+
+def draw_character(image, xy, char, color):
     cimg = Image.open(get_character_path(char))
+    if(color is not None):
+        cimg = replace_black(cimg,color)
     image.paste(cimg,xy,cimg)
     return 
 
@@ -21,10 +34,10 @@ def draw_country_ball(image,xy, country):
     image.paste(cimg,xy,cimg)
     return
 
-def draw_string(image,xy,string):
+def draw_string(image,xy,string, color=None):
     for i in range(len(string)):
         char = string[i]
         if(char == ' '):
             continue
         else:
-            draw_character(image,(xy[0] + 13*i, xy[1]),char)
+            draw_character(image,(xy[0] + 12*i, xy[1]),char,color)
