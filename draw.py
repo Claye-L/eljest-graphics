8a89077b9a80d0319f6d518363f90d51a5b8ad3e
Claye_L <claye@ixcys.fr>
2020-01-27 00:55:33 +0100

    demo 2

--- draw.py
+++ draw.py
@@ -6,7 +6,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\genImages"
 transparent = (0,0,0,0)
 
 country_size =(18,18)
-character_size = (13,16)
+character_size = (12,16)
+character_big_size=(24,31)
 
 class FontSize(Enum):
     Normal = ""
@@ -16,7 +17,11 @@ def get_character_path(char, fontsize):
 def get_country_path(country):
     return dir_path + "\\countries\\" + country +'.png'
 
-
+def char_width_for_font(fontsize):
+    if fontsize == FontSize.Normal:
+        return character_size[0]
+    elif fontsize == FontSize.Big:
+        return character_big_size[0]
 
 def replace_black(image, color):
     #code copied from https://stackoverflow.com/questions/3752476/python-pil-replace-a-single-rgba-color
@@ -29,7 +34,10 @@ def replace_black(image, color):
     return im2
 
 def draw_character(image, xy, char, color, fontsize = FontSize.Normal):
-    cimg = Image.open(get_character_path(char,fontsize))
+    try:
+        cimg = Image.open(get_character_path(char,fontsize))
+    except FileNotFoundError:
+        return
     if(color is not None):
         cimg = replace_black(cimg,color)
     image.paste(cimg,xy,cimg)
@@ -40,10 +48,22 @@ def draw_country_ball(image,xy, country):
     image.paste(cimg,xy,cimg)
     return
 
-def draw_string(image,xy,string, color=None, fontsize = FontSize.Normal ):
-    for i in range(len(string)):
-        char = string[i]
+def draw_centered_string(image, xybox, text, color = None, fontsize = FontSize.Normal):
+    if fontsize == FontSize.Normal:
+        char_width = character_size[0]
+    elif fontsize == FontSize.Big:
+        char_width = character_big_size[0]
+    center = (xybox[2] - xybox[0]) //2
+    textcenter = len(text) * char_width // 2
+    offset = center - textcenter
+    draw_string(image, (xybox[0] + offset, xybox[1], xybox[2], xybox),
+        text, color, fontsize)
+
+
+def draw_string(image,xy, text, color=None, fontsize = FontSize.Normal):
+    for i in range(len(text)):
+        char = text[i]
         if(char == ' '):
             continue
         else:
-            draw_character(image,(xy[0] + 12*i, xy[1]),char,color)
+            draw_character(image,(xy[0] + char_width_for_font(fontsize) * i, xy[1]),char,color, fontsize)
